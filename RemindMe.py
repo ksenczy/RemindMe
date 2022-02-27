b49690741d9b06d8c2459d7d905ca32fd4527725
Wesley Blanco <wblanco@bgsu.edu>
2022-02-27 17:49:34 -0500

    Added Create, Update Event. Holidays, Orgs

--- RemindMe.py
+++ RemindMe.py
@@ -30,6 +30,7 @@ window.geometry('1920x1080')
 window.configure(bg=brown)
 
 
+
 #This will be where the window changes from Login to the Home Screen
 def clickedLogin():
     #This if so we can show off a failed login
@@ -46,7 +47,6 @@ def convertTD(eventTime1):
     minutes = (seconds % 3600) // 60
     seconds = (seconds % 60)
     return hours, minutes, seconds
-
 
 
 #This is the function for the home screen
@@ -185,9 +185,9 @@ def Homescreen():
     #Matts photo and user stuff
     mattPic = Image.open(holder + "\Matthew.png")
     mattPicAnchor = ImageTk.PhotoImage(mattPic)
-    MattLabel1 = Label(image = mattPicAnchor).place(x = 1600, y = 700)
-    MattLabel2 = Label(text = "Matthew Lake",background = brown, foreground = "white", font = fontSettings).place(x = 1620, y = 980)
-    MattLabel3 = Label(text = "mrlake@bgsu.edu", background = brown, foreground = "white", font = fontSettings).place(x = 1620, y = 1020)
+    MattLabel1 = Label(image = mattPicAnchor).place(x = 1600, y = 650)
+    MattLabel2 = Label(text = "Matthew Lake",background = brown, foreground = "white", font = fontSettings).place(x = 1620, y = 940)
+    MattLabel3 = Label(text = "mrlake@bgsu.edu", background = brown, foreground = "white", font = fontSettings).place(x = 1620, y = 980)
     #again, this must be at the bottom of the Homescreen function to
     #make sure that things work
     #notably, photos dont work without this
@@ -227,8 +227,6 @@ def menuExtended(homeWindow):
     filtersEventImgAnchor = ImageTk.PhotoImage(filtersEventImg)
     settingsEventImg = Image.open(holder + "\Settings.png")
     settingsEventImgAnchor = ImageTk.PhotoImage(settingsEventImg)
-
-
 
 
     #Adding Buttons to the Frame for each menu option
@@ -274,14 +272,89 @@ def createEvent(isUpdateFunction, homeWindow):
     popUp.configure(bg=brown)
 
     #Setting up the top logo
-    topLogo = Label(popUp, text = labelWords, font=eventFont, background = brown, foreground = orange).place(x=300, y=50)
+    topLogo = Label(popUp, text = labelWords, font=eventFont, background = orange, width = 20, anchor = CENTER).place(x=180, y=25)
+
+    #Event Name Label and Event Date
+    eventNameLbl = Label(popUp, text = "Event Name:", background = brown, foreground = orange, font = fontSettings).place(x=140, y = 90)
+    eventNameEntry = Entry(popUp, width = 50).place(x=350, y=100)
+    eventDateLbl = Label(popUp, text = "Event Date:", background = brown, foreground = orange, font = fontSettings).place(x=140, y = 140)
+    eventDateEntry1 = Entry(popUp, width = 5)
+    eventDateEntry1.insert(0, "DD")
+    eventDateEntry1.place(x=350, y=150)
+    eventDateEntry2 = Entry(popUp, width = 5)
+    eventDateEntry2.insert(0, "MM")
+    eventDateEntry2.place(x=415, y=150)
+    eventDateEntry3 = Entry(popUp, width = 10)
+    eventDateEntry3.insert(0, "YYYY")
+    eventDateEntry3.place(x=480, y=150)
+    slashLine1 = Label(popUp, font = fontSettings, background = brown, foreground = "black", text = "/").place(x=395,y=145)
+    slashLine2 = Label(popUp, font = fontSettings, background = brown, foreground = "black", text = "/").place(x=460,y=145)
+
+    #Event Time and Comments
+    eventTimeLbl = Label(popUp, text = "Event Time:", background = brown, foreground = orange, font = fontSettings).place(x=140, y = 190)
+    eventTimeEntry1 = Entry(popUp, width = 5)
+    eventTimeEntry1.insert(0, "HH")
+    eventTimeEntry1.place(x=350, y=200)
+    eventTimeEntry2 = Entry(popUp, width = 5)
+    eventTimeEntry2.insert(0, "MM")
+    eventTimeEntry2.place(x=415, y=200)
+    eventTimeEntry3 = Combobox(popUp, width = 7)
+    eventTimeEntry3['values'] = ('AM', 'PM')
+    eventTimeEntry3.current(0)
+    eventTimeEntry3.place(x=480, y=200)
+    slashLine1 = Label(popUp, font = fontSettings, background = brown, foreground = "black", text = ":").place(x=395,y=190)
+    eventCommentsLbl = Label(popUp, text = "Comments:", background = brown, foreground = orange, font = fontSettings).place(x=140, y = 240)
+    eventCommentsEntry = Entry(popUp)
+    eventCommentsEntry.grid(padx = 350, pady = 250, ipadx = 50, ipady = 40)
 
 
+    #Recurring boxes and Notification Boxes
+    eventTimeLbl = Label(popUp, text = "Recurring:", background = brown, foreground = orange, font = fontSettings).place(x=140, y = 360)
+    daily = IntVar()
+    weekly = IntVar()
+    dailyCheckbox = Checkbutton(popUp, variable = daily, offvalue = 0, onvalue = 1, text = "Daily").place(x=350, y=370)
+    weeklyCheckbox = Checkbutton(popUp, variable = weekly, offvalue = 0, onvalue = 1, text = "Weekly").place(x=450, y=370)
+    eventNotifsLbl = Label(popUp, text = "Notifications:", background = brown, foreground = orange, font = fontSettings).place(x=140, y = 410)
+    notifs = IntVar()
+    notifsCheckbox = Checkbutton(popUp, variable = notifs, offvalue = 0, onvalue = 1, text = "On").place(x=350, y=420)
 
+    #Event Type Boxes
+    eventTimeLbl = Label(popUp, text = "Event Type:", background = brown, foreground = orange, font = fontSettings).place(x=140, y = 460)
+    classBtn = IntVar()
+    orgs = IntVar()
+    hmwrk = IntVar()
+    quiz = IntVar()
+    personal = IntVar()
+    holiday = IntVar()
+    other = IntVar()
+    classCheckbox = Checkbutton(popUp, variable = classBtn, offvalue = 0, onvalue = 1, text = "Class").place(x=350, y=470)
+    orgsCheckbox = Checkbutton(popUp, variable = orgs, offvalue = 0, onvalue = 1, text = "Organization").place(x=450, y=470)
+    hmwrkCheckbox = Checkbutton(popUp, variable = hmwrk, offvalue = 0, onvalue = 1, text = "Homework").place(x=350, y=500)
+    quizCheckbox = Checkbutton(popUp, variable = quiz, offvalue = 0, onvalue = 1, text = "Quizzes & Exams").place(x=450, y=500)
+    personalCheckbox = Checkbutton(popUp, variable = personal, offvalue = 0, onvalue = 1, text = "Personal").place(x=350, y=530)
+    holidayCheckbox = Checkbutton(popUp, variable = holiday, offvalue = 0, onvalue = 1, text = "Holiday").place(x=450, y=530)
+    otherCheckbox = Checkbutton(popUp, variable = other, offvalue = 0, onvalue = 1, text = "Other").place(x=400, y=560)
+
+    #Add Event Button
+    finalBtn = Button(popUp, text= labelWords, command= lambda : addDestroyer(isUpdateFunction, homeWindow, popUp),width = 20).place(x = 350, y = 650)
 
     buttonImage = Image.open(holder + "\Press Button.png")
     buttonImageAnchor = ImageTk.PhotoImage(buttonImage)
 
+
+    popUp.mainloop()
+
+#This is the clean up for the Add Event Function
+#This Kills the pop up and Adds to the home screen a new event
+#Or if its update, it updates the event
+def addDestroyer(isUpdate, homeWindow, popUp):
+    popUp.destroy()
+    if(isUpdate):
+        seventhEvent = Label(homeWindow, background = "green", text = "Haircut", font=eventFont, width = 35).place(x=525, y= 870)
+    else:
+        augustTwenth = Label(homeWindow, text="August 20th 2022", background = brown, foreground = "white", font=fontSettings).place(x=830, y = 820)
+        timeLeft4 = Label(homeWindow, text = "Time Left:", background = brown, foreground = "white", font = fontSettings).place(x=1120, y=820)
+        seventhEvent = Label(homeWindow, background = "green", text = "Bryce's Wedding", font=eventFont, width = 35).place(x=525, y= 870)
 
 
 #Function def for delete event pop up
@@ -307,11 +380,123 @@ def sharedWithMe():
 
 #function def for Organizations search
 def organizations():
-    garbage = 0
+    popUp = Tk()
+    popUp.title("Organization Search")
+    popUp.geometry('500x400')
+    popUp.configure(bg=brown)
+
+
+    #Setting up the Top logo
+    topOrg = Label(popUp, text = "Find Organization", font=eventFont, background = orange, width = 20, anchor = CENTER).place(x=20, y=20)
+    explain = Label(popUp, text = "Enter an Organization to get events for them", foreground = orange, background=brown, font=timeFont).place(x=35, y=80)
+    orgLbl = Entry(popUp, width = 50)
+    orgLbl.place(x=100, y=150)
+
+    #Button building
+    orgsButton = Button(popUp, text="Search", command= lambda : orgSearchFrame(popUp, orgLbl.get()),width = 20).place(x = 185, y = 200)
+
+    popUp.mainloop()
+
+#This is for responding to the search from Organizations definition
+def orgSearchFrame(popUp, searchTerm):
+    #Setting up the frame
+    coverUp= Frame(popUp)
+    coverUp.config()
+    coverUp.place(x=0,y=0, width = 500, height = 400)
+    #I couldnt color the background properly so this next line does it
+    #in a very, uh, less than ideal way
+    backgroundColor = Label(coverUp, font=("Times New Roman", 900), text="Hidden Text", background = brown, foreground = brown).place(x=0,y=0)
+
+    orgs = {'CS': ['ACM', 'WIC'], 'Sports': ['Hockey', 'Gymnastics'], 'Music': ['String Club', 'A Cappella Choir']}
+
+    isIncluded = False
+
+    foundLabel = Label(coverUp, text = "Find Organization", font=eventFont, background = orange, width = 20, anchor = CENTER).place(x=20, y=20)
+
+    for key in orgs:
+        if key == searchTerm:
+            isIncluded = True
+            orgFound = Label(coverUp, text = "We Found the following Organizations!", foreground = orange, background=brown, font=timeFont).place(x=35, y=80)
+            yourSearch = Label(coverUp, text="Your Search : " + searchTerm, foreground = orange, background = brown, font=timeFont).place(x=20, y=120)
+            myList = orgs[key]
+            firstOrg = Label(coverUp, background = "green", text = myList[0], font=eventFont, width = 15).place(x=0, y= 175)
+            secondOrg = Label(coverUp, background = "green", text = myList[1], font=eventFont, width = 15).place(x=0, y= 240)
+            chk1 = IntVar()
+            chk2 = IntVar()
+            cb1 = Checkbutton(coverUp, onvalue = 1, offvalue = 0, variable = chk1).place(x=400,y=190)
+            cb2 = Checkbutton(coverUp, onvalue = 1, offvalue = 0, variable = chk2).place(x=400,y=255)
+            addOrganizations = Button(coverUp, text = "Add Organizations", command = lambda : deletePopUp(popUp)).place(x=50, y=350)
+            tryAgainButton = Button(coverUp, text = "Search Again", command = lambda : deleteFrames(coverUp)).place(x=350, y=350)
+
+    if isIncluded != True:
+        failedSearchLbl = Label(coverUp, text = "No Organizations found!", foreground = orange, background=brown, font=timeFont).place(x=35, y=80)
+        tryAgainButton = Button(coverUp, text = "Search Again", command = lambda : deleteFrames(coverUp)).place(x=35, y=300)
+        cancelButton = Button(coverUp, text = "Add Organizations", command = lambda : deletePopUp(popUp)).place(x=350, y=350)
+
+
+    popUp.mainloop()
+
+
+#This function deletes frames
+def deleteFrames(frameName):
+    frameName.destroy()
+
+#this function deletes popups
+def deletePopUp(popUp):
+    popUp.destroy()
+
 
 #function Def for holiday settings
 def holidays():
-    garbage = 0
+    popUp = Tk()
+    popUp.title("Holidays")
+    popUp.geometry('500x400')
+    popUp.configure(bg=brown)
+
+    #Setting up the Vars
+    chk1 = IntVar()
+    chk2 = IntVar()
+    chk3 = IntVar()
+    chk4 = IntVar()
+    chk5 = IntVar()
+    chk6 = IntVar()
+    chk7 = IntVar()
+    chk8 = IntVar()
+    chk9 = IntVar()
+    chk10 = IntVar()
+    chk11 = IntVar()
+    chk12 = IntVar()
+    chk13 = IntVar()
+    chk14 = IntVar()
+
+    #Setting Up the top bar
+    holidayLabel = Label(popUp, text = "Holidays", font=eventFont, background = orange, width = 15, anchor = CENTER).place(x=75, y=20)
+
+    #Religions
+    christian = Checkbutton(popUp, variable = chk1, offvalue = 0, onvalue = 1, text = "Christianity").place(x=100, y=100)
+    hinduisim = Checkbutton(popUp, variable = chk2, offvalue = 0, onvalue = 1, text = "Hinduism").place(x=100, y=135)
+    taoism = Checkbutton(popUp, variable = chk3, offvalue = 0, onvalue = 1, text = "Taoism").place(x=100, y=170)
+    islam = Checkbutton(popUp, variable = chk4, offvalue = 0, onvalue = 1, text = "Islam").place(x=100, y=205)
+    judism = Checkbutton(popUp, variable = chk5, offvalue = 0, onvalue = 1, text = "Judism").place(x=100, y=240)
+    buddhism = Checkbutton(popUp, variable = chk6, offvalue = 0, onvalue = 1, text = "Buddhism").place(x=100, y=275)
+    sikhism = Checkbutton(popUp, variable = chk7, offvalue = 0, onvalue = 1, text = "Sikhism").place(x=100, y=310)
+
+    #Countries
+    usa = Checkbutton(popUp, variable = chk8, offvalue = 0, onvalue = 1, text = "United States").place(x=300, y=100)
+    india = Checkbutton(popUp, variable = chk9, offvalue = 0, onvalue = 1, text = "India").place(x=300, y=135)
+    china = Checkbutton(popUp, variable = chk10, offvalue = 0, onvalue = 1, text = "China").place(x=300, y=170)
+    saudiArabia = Checkbutton(popUp, variable = chk11, offvalue = 0, onvalue = 1, text = "Saudi Arabia").place(x=300, y=205)
+    canada = Checkbutton(popUp, variable = chk12, offvalue = 0, onvalue = 1, text = "Canada").place(x=300, y=240)
+    mexico = Checkbutton(popUp, variable = chk13, offvalue = 0, onvalue = 1, text = "Mexico").place(x=300, y=275)
+    uK = Checkbutton(popUp, variable = chk14, offvalue = 0, onvalue = 1, text = "United Kingdom").place(x=300, y=310)
+
+
+    #Deletion Button
+    addHolidays = Button(popUp, text = "Save Changes", command = lambda : deletePopUp(popUp)).place(x=200, y=350)
+
+
+    #Loop dec
+    popUp.mainloop()
 
 #function def for Filter settings
 def filters():
